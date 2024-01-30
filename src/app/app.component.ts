aa92978eefa59b9394314f849f5bd016d04eb4f9
joshuadada <jdbabatunde98@gmail.com>
2024-01-30 08:02:16 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'appx-ssr';
   currentYear: number = new Date().getFullYear()
 }
